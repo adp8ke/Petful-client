e48ffdc6ed3952682b550cf9794acfae9ee7dc14
Arpan Patel <adp8ke@gmail.com>
2019-10-16 13:53:36 -0400

    doing deploy stuff

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-
-    API_ENDPOINT: 'http://localhost:8080/api'
-    
+  // API_ENDPOINT: 'http://localhost:8080/api',
+  API_ENDPOINT: 'https://calm-basin-50709.herokuapp.com/api'
 };
