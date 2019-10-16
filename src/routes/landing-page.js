39ab0583838499663b9cd47f85f69d5fd92af1b0
Arpan Patel <adp8ke@gmail.com>
2019-10-16 14:23:08 -0400

    changed some stuff

--- src/routes/landing-page.js
+++ src/routes/landing-page.js
@@ -18,8 +18,7 @@ class LandingPage extends React.Component{
         <div className='landing-page-container'>
           <p>Welcome to APSPCA where our goal is make sure each animal
             can find their furever home. You can choose a dog or a cat, and the first available
-            pet will be available for you to adopt. If you cannot find a specific pet, you will have to start
-            the process over and go to the end of the queue. 
+            pet will be available for you to adopt. Pets are adoptable in a first come first serve manner.
           </p>
           <Link to='/adoption-page'>Adopt Now!</Link>
         </div>
